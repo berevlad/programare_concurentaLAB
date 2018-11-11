240ec7aebdd80f357a8227359546bdfcfef5b43e
berevlad <35237231+berevlad@users.noreply.github.com>
2018-11-11 23:07:17 +0200

    Add files via upload

---
+++ lab6/Lab/Lab/CriptareDecriptare.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    class CriptareDecriptare
+    {
+        
+
+        public static string Criptare(string text)
+        {
+            string textCriptat = "";
+            char randomChar;
+            Random rand = new Random();
+            for (int i = 0; i <= (text.Length - 1); i++)
+            {
+                randomChar = (char)(rand.Next(128));
+                textCriptat += ((char)(text[i] ^ randomChar)).ToString();
+                textCriptat += ((char)(randomChar ^ (128 - i))).ToString();
+            }            
+
+            return textCriptat;
+        }
+
+        public static string Decriptare(string textCriptat)
+        {
+            string textDecriptat = "";
+            char decriptat;
+            for (int i = 0; i < (textCriptat.Length - 1); i += 2)
+            {
+                decriptat = ((char)(textCriptat[i + 1] ^ (128 - (i + 1) / 2)));
+                textDecriptat += ((char)(textCriptat[i] ^ decriptat)).ToString();
+            }
+
+            return textDecriptat;
+        }
+    }
+}
---
+++ lab6/Lab/Lab/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Lab
+{
+   
+   
+
+    public partial class MainWindow : Window
+    {
+        static BackgroundWorker bkworker = new BackgroundWorker();
+        public static string textOriginal;
+        public static string textCriptat;
+        public List<string> textReasamblat = new List<string>();
+        public List<string> lista = new List<string>();
+        public static int numar;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void BtnThread_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+
+            textOriginal = Text_Initial.Text;
+            numar = textOriginal.Length / Environment.ProcessorCount;
+            lista.Capacity = Environment.ProcessorCount;
+            for (int i = 0; i < textOriginal.Length; i+=numar)
+            {
+                if ((i + numar) < textOriginal.Length)
+                {
+                    lista.Add(textOriginal.Substring(i, numar));
+                }
+                else
+                    lista.Add(textOriginal.Substring(i));
+            }
+            
+            for(int k = 0; k<lista.Count; k++)
+            {
+                int temp = k;
+                new Thread(() => { textReasamblat[temp] = CriptareDecriptare.Criptare(lista[temp]); }).Start();
+                
+            }
+            stopWatch.Start();
+            for(int i = 0; i<textReasamblat.Count; i++)
+                Criptat.Text += textReasamblat[i];
+            stopWatch.Stop();
+            Durata.Text = String.Format("{00:00} ms", stopWatch.ElapsedMilliseconds);
+        }
+
+        private void BtnExec_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            textOriginal = Text_Initial.Text;
+            stopWatch.Start();
+            textCriptat = CriptareDecriptare.Criptare(textOriginal);
+            stopWatch.Stop();
+            Durata.Text = String.Format("{00:00} ms", stopWatch.ElapsedMilliseconds);
+            Criptat.Text = textCriptat;
+        }
+
+
+    }
+}
